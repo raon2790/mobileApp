5daace112b569e51e7d1838a3c7cbde6ecc96d92
raon2790 <luckymoon4157@gmail.com>
2025-10-27 21:40:01 +0900

    커밋 테스트

--- app/src/main/java/com/example/composelab/MainActivity.kt
+++ app/src/main/java/com/example/composelab/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.composelab
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composelab.ui.theme.ComposeLabTheme
 
@@ -21,7 +24,7 @@ class MainActivity : ComponentActivity() {
             ComposeLabTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Compose",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -34,7 +37,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.background(Color.Green)
     )
 }
 
@@ -42,6 +45,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeLabTheme {
-        Greeting("Android")
+        Greeting("Compose")
     }
 }
