b3c12b997d682097cf949e2947dd8c14922e368a
raon2790 <luckymoon4157@gmail.com>
2025-10-29 03:11:34 +0900

    Add missing w01

---
+++ w01/src/main/java/com/example/w01/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.w01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.w01.ui.theme.JWTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JWTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize().padding(32.dp)) { innerPadding ->
+        Column(modifier = Modifier.fillMaxSize()) {
+            Text("My App started!!", modifier = Modifier.padding(16.dp))
+            Text("Hello Compose", modifier = Modifier.padding(16.dp).background(Color.Green))
+            Row {
+                Text("This is left.", modifier = Modifier.weight(1f))
+                Text("This is right.", modifier = Modifier.weight(1f))
+            }
+            Button(onClick = {}) {
+                Text("Click me!!")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    JWTheme {
+        MainScreen()
+    }
+}
